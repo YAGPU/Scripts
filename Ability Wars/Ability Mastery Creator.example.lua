2d59f4c41ffc2ca099389f08a23d55a9f6974829
YAGPU <140895742+YAGPU@users.noreply.github.com>
2023-08-31 17:10:29 +0300

    Create Ability Mastery Creator.example.lua

---
+++ Ability Wars/Ability Mastery Creator.example.lua
@@ -0,0 +1,23 @@
+local Library = loadstring(game:HttpGet(“https://raw.githubusercontent.com/YAGPU/Scripts/main/Ability%20Wars/Ability%20Mastery%20Creator.lua”, true))()
+
+local MasteryTable = {
+  E = false
+  Q = true,
+  R = true,
+
+  Base = "Gravity",
+  
+  QF = function()
+    workspace.Gravity += 40
+  end,
+
+  RF = function()
+    workspace.Gravity -= 40
+  end,
+
+  onDeath = function()
+    workspace.Gravity = 196.2
+  end
+}
+
+Library:SetupMastery(MasteryTable)
